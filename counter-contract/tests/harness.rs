14e575868fbc8bba6657813f7fe1232589610ad1
Randula <randula@linearsix.com>
2024-03-03 10:01:55 +0530

    harness test added

--- counter-contract/tests/harness.rs
+++ counter-contract/tests/harness.rs
@@ -1,11 +1,11 @@
 use fuels::{prelude::*, types::ContractId};
-
+ 
 // Load abi from json
 abigen!(Contract(
     name = "MyContract",
     abi = "out/debug/counter-contract-abi.json"
 ));
-
+ 
 async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
     // Launch a local network and deploy the contract
     let mut wallets = launch_custom_provider_and_get_wallets(
@@ -20,7 +20,7 @@ async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
     .await
     .unwrap();
     let wallet = wallets.pop().unwrap();
-
+ 
     let id = Contract::load_from(
         "./out/debug/counter-contract.bin",
         LoadConfiguration::default(),
@@ -29,15 +29,30 @@ async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
     .deploy(&wallet, TxPolicies::default())
     .await
     .unwrap();
-
+ 
     let instance = MyContract::new(id.clone(), wallet);
-
+ 
     (instance, id.into())
 }
-
+ 
 #[tokio::test]
 async fn can_get_contract_id() {
     let (_instance, _id) = get_contract_instance().await;
-
+ 
     // Now you have an instance of your contract you can use to test each function
+}
+ 
+#[tokio::test]
+async fn test_increment() {
+    let (instance, _id) = get_contract_instance().await;
+ 
+    // Increment the counter
+    instance.methods().increment().call().await.unwrap();
+ 
+    // Get the current value of the counter
+    let result = instance.methods().count().call().await.unwrap();
+ 
+    // Check that the current value of the counter is 1.
+    // Recall that the initial value of the counter was 0.
+    assert_eq!(result.value, 1);
 }
